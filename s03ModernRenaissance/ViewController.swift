ebe03935c3ee7e48bcadb8172bcb315dc4809e55
BP <oh_strawberry@yahoo.com>
2017-01-09 09:09:49 -0800

    Updated msg

--- s03ModernRenaissance/ViewController.swift
+++ s03ModernRenaissance/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hallo there I am hungry")
+        print("Hallo there I am hungry. So hungry.")
         
     }
 
